4f5a1d5067d1de682216a66dd5eb5062c24c1a71
Sidda Chalamalasetty <tosidda@gmail.com>
2020-03-16 18:06:31 -0400

    Updates

---
+++ precache-manifest.c86216e32d185c0d426ca54988167629.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911384b4e1eddb9fadc6bfc4b88733c8",
+    "url": "/covid-react/index.html"
+  },
+  {
+    "revision": "a474f70f06c103ee3092",
+    "url": "/covid-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c05860e6972befe8fd0",
+    "url": "/covid-react/static/js/2.a7463ed3.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/covid-react/static/js/2.a7463ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a474f70f06c103ee3092",
+    "url": "/covid-react/static/js/main.6f86d8a9.chunk.js"
+  },
+  {
+    "revision": "6d077b3b0d50e050c85e",
+    "url": "/covid-react/static/js/runtime-main.52b36900.js"
+  },
+  {
+    "revision": "4a33bab6b9d4f19945ebb3c0e7684e81",
+    "url": "/covid-react/static/media/beer.4a33bab6.png"
+  },
+  {
+    "revision": "a735d38a0aa165409e23e39b31116e6e",
+    "url": "/covid-react/static/media/corana.a735d38a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
